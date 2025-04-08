e5a793092354ac3f85eb15cfed2a387ff9836e9a
hoahamhoc <vanhoa.29012000@gmail.com>
2025-04-08 11:57:55 +0700

    ct rename column

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,25 @@
+
+-- B1: Source lấy data
+WITH dim_product_source AS
+(
+  SELECT 
+   *
+  FROM 
+  `vit-lam-data.wide_world_importers.warehouse__stock_items`
+),
+--B2: Đổi cột
+  dim_product_renname_column AS
+  (
+SELECT
+stock_item_id as product_key,
+stock_item_name as product_name,
+brand as brand_name
+FROM 
+dim_product_source
+)
+
 SELECT  
-CAST (stock_item_id AS INTEGER) as product_key,
-CAST(stock_item_name AS STRING) as product_name,
-CAST (brand as STRING ) AS brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items` 
+CAST(product_key AS INTEGER) as product_key,
+CAST(product_name AS STRING) as product_name,
+CAST (brand_name as STRING ) as brand_name
+FROM dim_product_renname_column
