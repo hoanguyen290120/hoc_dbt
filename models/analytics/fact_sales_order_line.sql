8e4530deacf4c4e71e81f2d1a7db7cf39b476a8d
hoahamhoc <vanhoa.29012000@gmail.com>
2025-04-08 11:09:06 +0700

    add measure

---
+++ models/analytics/fact_sales_order_line.sql
@@ -0,0 +1,5 @@
+SELECT order_line_id as sales_order_line_key,
+       quantity,
+       unit_price,
+       unit_price*quantity as gross_amount
+ FROM `vit-lam-data.wide_world_importers.sales__order_lines` 
