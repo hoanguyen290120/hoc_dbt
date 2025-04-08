ea3e8033b0d7acffb3243982c98720bb4676fddf
hoahamhoc <vanhoa.29012000@gmail.com>
2025-04-08 14:18:33 +0700

    add cte

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,41 @@
+--B1: Đổi source lấy data
+WITH fact_sales_order_line_source AS
+(
 SELECT
-CAST( order_line_id AS INTEGER) as sales_order_line_key,
-CAST (stock_item_id AS INTEGER) as product_key,
+ * 
+FROM 
+`vit-lam-data.wide_world_importers.sales__order_lines`
+),
+
+--Đổi cột
+sales_order_line_rename_column AS
+(
+
+  SELECT
+  order_line_id as sales_order_line_key ,
+  stock_item_id as product_key,
+  quantity as quantity,
+  unit_price as unit_price,
+  FROM 
+  fact_sales_order_line_source
+),
+
+--B3: Chuyển type
+fact_sales_order_line_change_type AS
+(
+SELECT
+CAST( sales_order_line_key AS INTEGER) as sales_order_line_key,
+CAST (product_key AS INTEGER) as product_key,
  CAST(quantity AS INTEGER) as quantity,
-CAST(unit_price AS NUMERIC) as unit_price,
-CAST(unit_price as INTEGER)*CAST(quantity as NUMERIC) as gross_amount
- FROM `vit-lam-data.wide_world_importers.sales__order_lines` 
+CAST(unit_price AS NUMERIC) as unit_price
+FROM
+sales_order_line_rename_column
+)
+SELECT 
+sales_order_line_key,
+product_key,
+quantity,
+unit_price,
+unit_price*quantity as gross_amount
+FROM 
+fact_sales_order_line_change_type
