e5c58e78126bec80c21e9eb581660a809898f319
hoahamhoc <vanhoa.29012000@gmail.com>
2025-04-08 11:36:12 +0700

    add format

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,6 +1,7 @@
-SELECT order_line_id as sales_order_line_key,
-stock_item_id as product_key,
-       quantity,
-       unit_price,
-       unit_price*quantity as gross_amount
+SELECT
+CAST( order_line_id AS INTEGER) as sales_order_line_key,
+CAST (stock_item_id AS INTEGER) as product_key,
+ CAST(quantity AS INTEGER) as quantity,
+CAST(unit_price AS NUMERIC) as unit_price,
+CAST(unit_price as INTEGER)*CAST(quantity as NUMERIC) as gross_amount
  FROM `vit-lam-data.wide_world_importers.sales__order_lines` 
